96e87102ab6ad3849a11e425f25bf6b7f07ccda6
t.kurtovic <tk50908@fer.hr>
2021-03-14 00:32:44 +0100

    Test for result with 2 strikes in a row at the beginning and other frames numbers

--- hw_1/bowling.js
+++ hw_1/bowling.js
@@ -3,25 +3,58 @@ const frameResultAfterTwoRolls = () => {
     return 10
 }
 
+const cleanUpGameFrame = (gameFrame) => {
+    var gameFrame = gameFrame.replace('-', '');
+    return gameFrame;
+}
+
+const multipleStrikesScore = (gameFrames, gameFrame) => {
+    let score = 10
+    let i = gameFrames.lastIndexOf(gameFrame) + 1;
+    let nextGameFrame = cleanUpGameFrame(gameFrames[i]);
+    console.log(nextGameFrame)
+    nextGameFrame.split().forEach((roll) => {
+        score += parseInt(roll);
+    })
+    return score;
+}
+
+const normalScore = (gameFrame) => {
+    let score = 0
+    gameFrame.split().forEach((roll) => {
+        score += parseInt(roll);
+    })
+    return score;
+}
+
+const STRIKE = 'X'
+const SPARE = '/'
+
 const gameResult = (game) => {
     const gameFrames = game.trim().split(" ")
-    console.log(gameFrames)
     var score = 0;
     var lastFrame = '';
     gameFrames.forEach((gameFrame) => {
-    
-        if(lastFrame === 'X'){
+
+        cleanUpGameFrame(gameFrame)
+
+        if (lastFrame === STRIKE || lastFrame === SPARE) {
             score += 10;
-            gameFrame.split().forEach((roll) => {
-                score += parseInt(roll);
-            })
+
+            if (gameFrame === STRIKE) {
+                score += multipleStrikesScore(gameFrames, gameFrame)
+            } else {
+                if (lastFrame === SPARE) {
+                    score += normalScore(gameFrame[0])
+                } else {
+                    score += normalScore(gameFrame)
+                }
+            }
         }
 
-        if(gameFrame !== 'X')
-            gameFrame.split().forEach((roll)=> {
-                if(roll !== '-') 
-                    score += parseInt(roll);
-            })
+        if (gameFrame !== STRIKE && gameFrame !== SPARE) {
+            score += normalScore(gameFrame)
+        }
 
         lastFrame = gameFrame;
     })
--- hw_1/bowling.test.js
+++ hw_1/bowling.test.js
@@ -2,7 +2,6 @@ const { it, expect } = require("@jest/globals");
 
 const bowling = require('./bowling');
 
-
 it('When given 1 and 9 return 10', () => {
     const result =bowling.frameResultAfterTwoRolls('19');
     expect(result).toEqual(10);
@@ -16,4 +15,23 @@ it('When given 1- 1- 1- 1- 1- 1- 1- 1- 1- 1- return 10', () => {
 it('When given X 1- 1- 1- 1- 1- 1- 1- 1- 1- return 20', () => {
     const result = bowling.gameResult('X ' + '1- '.repeat(9));
     expect(result).toEqual(20);
+})
+
+it('When given / 2- 2- 2- 2- 2- 2- 2- 2- 2- return 30', () => {
+    const result = bowling.gameResult('/ ' + '2- '.repeat(9))
+    expect(result).toEqual(30);
+})
+
+const sequenceWithMinusFromOneTo = (upperLimit) => {
+    let sequence = ''
+    for(let i = 1; i < upperLimit + 1; i++){
+        sequence += `${i}- `
+    }
+    return sequence;
+}
+
+it('When given X X 1- 2- 3- 4- 5- 6- 7- 8- return 68', () => {
+    const bowlingGame = 'X '.repeat(2) + sequenceWithMinusFromOneTo(8);
+    const result = bowling.gameResult(bowlingGame);
+    expect(result).toEqual(68);
 })
