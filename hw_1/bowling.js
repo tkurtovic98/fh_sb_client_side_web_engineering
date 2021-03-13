929e0594b7721bcb92a6f61feae6db9ba56911d4
t.kurtovic <tk50908@fer.hr>
2021-03-13 23:37:21 +0100

    Test for game result with first frame strike and other frames numbers

--- hw_1/bowling.js
+++ hw_1/bowling.js
@@ -3,8 +3,30 @@ const frameResultAfterTwoRolls = () => {
     return 10
 }
 
-const gameResult = () => {
-    return 10
+const gameResult = (game) => {
+    const gameFrames = game.trim().split(" ")
+    console.log(gameFrames)
+    var score = 0;
+    var lastFrame = '';
+    gameFrames.forEach((gameFrame) => {
+    
+        if(lastFrame === 'X'){
+            score += 10;
+            gameFrame.split().forEach((roll) => {
+                score += parseInt(roll);
+            })
+        }
+
+        if(gameFrame !== 'X')
+            gameFrame.split().forEach((roll)=> {
+                if(roll !== '-') 
+                    score += parseInt(roll);
+            })
+
+        lastFrame = gameFrame;
+    })
+
+    return score
 }
 
 module.exports = {
--- hw_1/bowling.test.js
+++ hw_1/bowling.test.js
@@ -9,6 +9,11 @@ it('When given 1 and 9 return 10', () => {
 })
 
 it('When given 1- 1- 1- 1- 1- 1- 1- 1- 1- 1- return 10', () => {
-    const result = bowling.gameResult('1- ' * 10);
+    const result = bowling.gameResult('1- '.repeat(10));
     expect(result).toEqual(10);
+})
+
+it('When given X 1- 1- 1- 1- 1- 1- 1- 1- 1- return 20', () => {
+    const result = bowling.gameResult('X ' + '1- '.repeat(9));
+    expect(result).toEqual(20);
 })
